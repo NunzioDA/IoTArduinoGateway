dc920af12a0552d09a4d5b2ed379f83bc27c3ff2
NunzioDA <nunziodamore99@gmail.com>
2025-05-03 22:31:20 +0200

    first commit

---
+++ listeners.py
@@ -0,0 +1,27 @@
+from arduino_interface import ArduinoListener
+from tools import WebServer
+
+# This class describes the listener that
+# manages the info request response
+class InfoManager(ArduinoListener):
+    def __init__(self):
+        self.info = None
+        self.waiting_info = False
+
+    def on_message(self, message):
+        self.on_info(message[1])
+        self.waiting_info = False
+
+    def on_info(self, info):
+        # sending info to the server
+        print(f"Info received: {info}")
+        r = WebServer.update_status(info)
+
+# This is the listener that manages the
+# arduino response to a general command
+class CommandResponse(ArduinoListener):
+    def __init__(self):
+        self.info = None
+
+    def on_message(self, message):
+        print(f"received: {message[1]}")
