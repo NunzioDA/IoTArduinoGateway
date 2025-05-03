1cc4d3fc8bfce61137606446f7c760a9becc4b88
NunzioDA <nunziodamore99@gmail.com>
2025-05-03 22:30:58 +0200

    using .env to get password and backend address

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
 import json
 import time
-from arduino_interface import Arduino, ArduinoListener
-import requests
-
-base_url = "https://www.coinquilinipercaso.altervista.org/IoTProject/"
-command_queue_file = "get_command_queue.php"
-status_script_file = "update_status.php"
+from arduino_interface import Arduino
+from listeners import CommandResponse, InfoManager
+from tools import WebServer
 
 
+# Initializing the webserver
+WebServer.init()
+
+# Connecting to arduino
+Arduino.connect("COM5")
 last_command_timestamp = None
-
-class InfoManager(ArduinoListener):
-    def __init__(self):
-        self.info = None
-        self.waiting_info = False
-
-    def on_message(self, message):
-        self.on_info(message[1])
-        self.waiting_info = False
-
-    def on_info(self, info):
-        # sending info to the server
-        print(f"Info received: {info}")
-        r = requests.get(base_url+status_script_file, {"info": info, "password": "_riNzDo27P29oco"})
-        print(r.text)
-
-class CommandResponse(ArduinoListener):
-    def __init__(self):
-        self.info = None
-
-    def on_message(self, message):
-        print(f"received: {message[1]}")
-
 info_manager = InfoManager()
 
-Arduino.connect("COM3")
-
+# Main thread loop
+# This loop will check for new commands in the queue
+# sending them to arduino.
+# It also update the enviroment status by making an info
+# request to arduino and loading the result on the webserver.
 while True:
-    command_queue = requests.get(base_url+command_queue_file, {"password": "_riNzDo27P29oco", "timestamp": last_command_timestamp or "2015-01-01 00:00:00"})
+    # Getting the command queue
+    command_queue = WebServer.get_command_queue(
+        timestamp=last_command_timestamp
+    )
+    print(command_queue.text)
     command_queue_json = json.loads(command_queue.text)
 
+    # Checking for new commands in the queue
     for command in command_queue_json:
         # Check if the command is given later than the last command
         if last_command_timestamp is None or command["timestamp"] > last_command_timestamp:
@@ -47,6 +34,7 @@ while True:
             command = command["command"]
             Arduino.send(command, CommandResponse())
     
+    # Updating the enviroment status
     if not info_manager.waiting_info:
         Arduino.send("info", info_manager)
         info_manager.waiting_info = True
